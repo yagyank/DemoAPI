e0552102502ad9b35afff5fd251bcc31c66a614c
YAGYANK CHADHA <yagyankchadha@YAGYANKs-MacBook-Pro.local>
2017-07-03 21:26:25 +0800

    fixed API version

--- API/src/main/java/API.java
+++ API/src/main/java/API.java
@@ -1,6 +1,7 @@
 
 import java.awt.PageAttributes.MediaType;
 import java.io.IOException;
+import java.util.Map;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -11,9 +12,11 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
 
 import com.coinbase.api.Coinbase;
 import com.coinbase.api.CoinbaseBuilder;
+import com.coinbase.api.entity.Quote;
 import com.coinbase.api.entity.Response;
 import com.coinbase.api.exception.CoinbaseException;
 import com.coinbase.api.exception.UnspecifiedAccount;
@@ -22,9 +25,13 @@ public class API {
 
 	public static void main(String args[]) throws UnspecifiedAccount, IOException, CoinbaseException {
 		Coinbase cb = new CoinbaseBuilder()
-                .withApiKey("uYSEHgT38vXCluK8", "BnVcgmzsNaxrQx5gE6shv3nUpP2HtvK7")
+                .withApiKey("TdJP32LPFcXdQbbD", "1kzhSNVFoMsLD17h7BFTW5sUKxg97lja")
                 .build();
-			System.out.print(cb.getBuyQuote(1000));
+		System.out.println(cb.getSpotPrice(CurrencyUnit.SGD));
+		Quote q = cb.getBuyQuote(Money.parse("BTC 1"));
+		Map<String, Money> fees = q.getFees();
+		System.out.println(q.getSubtotal());
+		System.out.println(q.getTotal());
 	//	  .get();
 	}
 }
